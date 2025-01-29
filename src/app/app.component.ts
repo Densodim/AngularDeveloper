380de9ed8853f6ce6a6bcf4abf0fe1ae36984529
Densodim <25denso@gmail.com>
2025-01-29 16:30:21 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UploadComponent} from './upload/upload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UploadComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.less'
 })
 export class AppComponent {
---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Upload',
+  imports: [],
+  templateUrl: './upload.component.html',
+  standalone: true,
+  styleUrl: './upload.component.less'
+})
+export class UploadComponent {
+
+}
